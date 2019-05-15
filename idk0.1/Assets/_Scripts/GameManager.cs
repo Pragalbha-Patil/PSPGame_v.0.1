0b811da57d9dabb8750bc3c63c308b1252be7252
Pragalbha Patil <pragalbha.p@somaiya.edu>
2019-05-15 15:14:21 +0530

    PSPGame_v.0.1 [Alpha]
    My first game! :100:

---
+++ idk0.1/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    public float restartDelay = 1f;
+    public void EndGame()
+    {
+        if (!gameHasEnded)
+        {
+            gameHasEnded = true;
+            Debug.Log("Game over!");
+            //Restart();
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    void Restart()
+    {
+        //SceneManager.LoadScene("Level01");
+        //OR
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); //Returns the active scene
+    }
+}
